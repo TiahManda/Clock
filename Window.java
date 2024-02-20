3a4acb85c7dca79fd780b7f172f26b9cc9ca65f5
TiahManda <mandaniainatiaheranto@gmail.com>
2024-02-20 07:26:21 +0300

    update window.java | display of the main window

--- Window.java
+++ Window.java
@@ -1,1 +1,19 @@
-import javax.swing
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+    public Window() {
+
+        // window property
+        this.setTitle("Clock");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(000,000);
+        this.setResizable(true);
+        this.setSize(400,500);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Window window = new Window();
+    }
+}
